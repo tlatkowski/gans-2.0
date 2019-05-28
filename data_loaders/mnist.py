a36b424af74149e20483c53fc3628de2e7751e1d
Tomasz Latkowski <tlatkowski@gmail.com>
2019-05-28 20:32:30 +0200

    mnist dataset

---
+++ data_loaders/mnist.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+train_images = (train_images - 127.5) / 127.5 # Normalize the images to [-1, 1]
+
+BUFFER_SIZE = 60000
+BATCH_SIZE = 256
+
+# Batch and shuffle the data
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
