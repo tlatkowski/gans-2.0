8e58caca64ecc41efab20d7197bc65c9bf2d71dc
Tomasz Latkowski <tlatkowski@gmail.com>
2019-05-28 19:38:59 +0200

    added losses

---
+++ layers/losses.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+cross_entropy = tf.keras.losses.binary_crossentropy(from_logits=True)
+
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
