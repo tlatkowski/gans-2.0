461658ea12d166e82bbf79536e40e7608b596c4d
Tomasz Latkowski <tlatkowski@gmail.com>
2019-06-06 22:54:47 +0200

    changes in coco dataset reader

--- data_loaders/coco.py
+++ data_loaders/coco.py
@@ -1,15 +1,21 @@
-import json
 import io
+import json
+
 from tensorflow.python.keras import preprocessing
 
 CAPTIONS_DIR = '../annotations/captions_val2017.json'
-IMAGES_DIR = 'val2017'
+IMAGES_DIR = '../val2017'
 
-caption_file = io.open(CAPTIONS_DIR)
 
-caption_json = json.load(caption_file)
+def load_json_captions(path: str):
+    caption_file = io.open(path)
+    caption_json = json.load(caption_file)
+    return caption_json
 
-annotations = caption_json["annotations"]
+
+captions_json = load_json_captions(CAPTIONS_DIR)
+
+annotations = captions_json["annotations"]
 annotation_count = len(annotations)
 tokenizer = preprocessing.text.Tokenizer(num_words=10)
 
@@ -17,5 +23,8 @@ for annotation in annotations:
     image_id = annotation["image_id"]
     caption = annotation["caption"]
     tokenizer.fit_on_texts(caption)
-    a = tokenizer.texts_to_sequences(caption)
+    vectorized_caption = tokenizer.texts_to_sequences(caption)
     print(str(image_id) + ": " + caption)
+    
+    image_fn = "{:0>12}.jpg".format(image_id)
+    print(image_fn)
