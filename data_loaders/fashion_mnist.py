e3f99e18956275f8ed5e4be77db3c29c3f26ff2a
Tomasz Latkowski <tlatkowski@gmail.com>
2019-07-29 11:20:28 +0200

    further impl of fashion dataset

--- data_loaders/fashion_mnist.py
+++ data_loaders/fashion_mnist.py
@@ -6,6 +6,7 @@ def load_data(input_params):
     fashion_mnist = datasets.fashion_mnist
     (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
     # return (train_images, train_labels), (test_images, test_labels)
+    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
     train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(
         input_params.buffer_size).batch(
         input_params.batch_size)
