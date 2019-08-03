d3d0cdf0ceafb56ea472508549873f931e14febd
Tomasz Latkowski <tlatkowski@gmail.com>
2019-08-03 09:02:31 +0200

    make gifs

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,3 +1,24 @@
+import PIL
+import glob
+import imageio
 
-def make_gif_from_images():
-    pass
+
+def make_gif_from_images(anim_file='dcgan.gif'):
+    with imageio.get_writer(anim_file, mode='I') as writer:
+        filenames = glob.glob('image*.png')
+        filenames = sorted(filenames)
+        last = -1
+        for i, filename in enumerate(filenames):
+            frame = 2 * (i ** 0.5)
+            if round(frame) > round(last):
+                last = frame
+            else:
+                continue
+            image = imageio.imread(filename)
+            writer.append_data(image)
+        image = imageio.imread(filename)
+        writer.append_data(image)
+
+
+def display_image(epoch_no):
+    return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
