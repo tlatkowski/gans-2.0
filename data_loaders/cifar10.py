747a879711f2afae7f2328a69c5c90dc72ab5aa6
Tomasz Latkowski <tlatkowski@gmail.com>
2019-08-15 16:13:48 +0200

    changes in cifar10 load data

--- data_loaders/cifar10.py
+++ data_loaders/cifar10.py
@@ -4,7 +4,7 @@ from tensorflow.python.keras import datasets
 
 def load_data(input_params):
     cifar10 = datasets.cifar10
-    (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
+    (train_images, train_labels), (_, _) = cifar10.load_data()
     train_images = train_images.astype('float32')
     train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
     train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(
