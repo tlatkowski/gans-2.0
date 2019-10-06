5b5b97f9a4d947abbdb8bb9307a722c6938460c6
Tomasz Latkowski <tlatkowski@gmail.com>
2019-10-06 16:09:52 +0200

    changes in discriminators

--- models/conditional_gan.py
+++ models/conditional_gan.py
@@ -1,6 +1,6 @@
 from easydict import EasyDict as edict
 
-from models import discriminators
+from models.discriminators import cifar10_conditional_discriminator
 from trainers import conditional_gan_trainer
 from utils import model_utils
 
@@ -18,7 +18,8 @@ class ConditionalGAN:
         self.problem_type = input_args.problem_type
         
         self.generator = model_utils.generator_model_factory(input_params, self.problem_type)
-        self.discriminator = discriminators.ConditionalDiscriminatorCifar10(input_params)
+        self.discriminator = cifar10_conditional_discriminator.ConditionalDiscriminatorCifar10(
+            input_params)
         self.conditional_gan_trainer = conditional_gan_trainer.ConditionalGANTrainer(
             self.batch_size,
             self.generator,
