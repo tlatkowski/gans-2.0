360b1040c979edf47078ed4ee31966699ef79f78
Tomasz Latkowski <tlatkowski@gmail.com>
2019-10-07 14:56:12 +0200

    changes in vanilla gan

--- models/vanilla_gan.py
+++ models/vanilla_gan.py
@@ -1,6 +1,6 @@
 from easydict import EasyDict as edict
 
-from models import discriminators
+from models.discriminators import basic_discriminator
 from trainers import vanilla_gan_trainer
 from utils import model_utils
 
@@ -13,7 +13,7 @@ class VanillaGAN:
         self.problem_type = input_args.problem_type
         
         self.generator = model_utils.generator_model_factory(input_params, self.problem_type)
-        self.discriminator = discriminators.Discriminator(input_params)
+        self.discriminator = basic_discriminator.Discriminator(input_params)
         self.vanilla_gan_trainer = vanilla_gan_trainer.VanillaGANTrainer(self.batch_size,
                                                                          self.generator,
                                                                          self.discriminator,
--- trainers/vanilla_gan_trainer.py
+++ trainers/vanilla_gan_trainer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+from tqdm import tqdm
 
 from layers import losses
 from trainers import gan_trainer
@@ -30,26 +31,32 @@ class VanillaGANTrainer(gan_trainer.GANTrainer):
                 print('No checkpoints found. Starting training from scratch.')
         latest_epoch = latest_checkpoint_epoch * self.checkpoint_step
         epochs += latest_epoch
-        for epoch in range(latest_epoch, epochs):
-            print(epoch)
-            for image_batch in dataset:
-                train_step += 1
-                gen_loss, dis_loss = self.train_step(image_batch)
+        for epoch in tqdm(range(latest_epoch, epochs), desc='Epochs'):
+            num_batches = 390
+            tqdm_batch_iter = tqdm(range(num_batches), total=num_batches, desc="Batches",
+                                   leave=False)
+            # for image_batch in dataset:
+            with tqdm_batch_iter as progress_bar:
+                for image_batch in dataset:
+                    train_step += 1
+                    gen_loss, dis_loss = self.train_step(image_batch)
+                    with self.summary_writer.as_default():
+                        tf.summary.scalar("generator_loss", gen_loss, step=train_step)
+                        tf.summary.scalar("discriminator_loss", dis_loss, step=train_step)
+                    progress_bar.update(1)
+                
+                img_to_plot = visualization.generate_and_save_images(self.generator,
+                                                                     epoch + 1,
+                                                                     test_seed,
+                                                                     self.dataset_type,
+                                                                     cmap='gray')
                 with self.summary_writer.as_default():
-                    tf.summary.scalar("generator_loss", gen_loss, step=train_step)
-                    tf.summary.scalar("discriminator_loss", dis_loss, step=train_step)
-            
-            img_to_plot = visualization.generate_and_save_images(self.generator,
-                                                                 epoch + 1,
-                                                                 test_seed,
-                                                                 self.dataset_type,
-                                                                 cmap='gray')
-            with self.summary_writer.as_default():
-                tf.summary.image('test_images', np.reshape(img_to_plot, newshape=(1, 480, 640, 4)),
-                                 step=epoch)
-            
-            if (epoch + 1) % self.checkpoint_step == 0:
-                self.checkpoint.save(file_prefix=self.checkpoint_prefix)
+                    tf.summary.image('test_images',
+                                     np.reshape(img_to_plot, newshape=(1, 480, 640, 4)),
+                                     step=epoch)
+                
+                if (epoch + 1) % self.checkpoint_step == 0:
+                    self.checkpoint.save(file_prefix=self.checkpoint_prefix)
     
     @tf.function
     def train_step(self, real_images, generator_inputs=None):
