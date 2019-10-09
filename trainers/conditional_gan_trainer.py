6d3f3217c9ea706048f49d04f138cdcd981bd446
Tomasz Latkowski <tlatkowski@gmail.com>
2019-10-09 23:03:00 +0200

    removed tqdm

--- trainers/conditional_gan_trainer.py
+++ trainers/conditional_gan_trainer.py
@@ -1,6 +1,5 @@
 import numpy as np
 import tensorflow as tf
-from tqdm import tqdm
 
 from layers import losses
 from trainers import gan_trainer
@@ -34,21 +33,14 @@ class ConditionalGANTrainer(gan_trainer.GANTrainer):
             latest_checkpoint_epoch = int(latest_checkpoint[latest_checkpoint.index("-") + 1:])
         latest_epoch = latest_checkpoint_epoch * self.checkpoint_step
         epochs += latest_epoch
-        # for epoch in tqdm(range(latest_epoch, epochs), desc='Epochs'):
         for epoch in range(latest_epoch, epochs):
-            num_batches = 390
-            # tqdm_batch_iter = tqdm(range(num_batches), total=num_batches, desc="Batches", leave=False)
-            # for image_batch in dataset:
-            # with tqdm_batch_iter as progress_bar:
             for image_batch in dataset:
                 train_step += 1
                 gen_loss, dis_loss = self.train_step(image_batch)
                 with self.summary_writer.as_default():
                     tf.summary.scalar("generator_loss", gen_loss, step=train_step)
                     tf.summary.scalar("discriminator_loss", dis_loss, step=train_step)
-
-                    # progress_bar.update(1)
-                
+            
             img_to_plot = visualization.generate_and_save_images(self.generator, epoch + 1,
                                                                  test_seed,
                                                                  self.dataset_type,
@@ -58,7 +50,6 @@ class ConditionalGANTrainer(gan_trainer.GANTrainer):
                                  step=epoch)
             if (epoch + 1) % self.checkpoint_step == 0:
                 self.checkpoint.save(file_prefix=self.checkpoint_prefix)
-
     
     @tf.function
     def train_step(self, real_images):
