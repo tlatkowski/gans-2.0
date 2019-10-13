fa8950811e3f82e780d590b24e03a5bff7b64dcd
Tomasz Latkowski <tlatkowski@gmail.com>
2019-10-14 00:12:01 +0200

    extracted norm inputs

--- data_loaders/cifar10.py
+++ data_loaders/cifar10.py
@@ -2,11 +2,22 @@ import tensorflow as tf
 from tensorflow.python.keras import datasets
 
 
+def normalize_inputs(data):
+    """
+    Normalizes the inputs to [-1, 1]
+    
+    :param data:
+    :return:
+    """
+    data = (data - 127.5) / 127.5  #
+    return data
+
+
 def load_data(input_params):
     cifar10 = datasets.cifar10
     (train_images, train_labels), (_, _) = cifar10.load_data()
     train_images = train_images.astype('float32')
-    train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
+    train_images = normalize_inputs(train_images)
     train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(
         input_params.buffer_size).batch(
         input_params.batch_size)
@@ -17,7 +28,7 @@ def load_data_with_labels(input_params):
     cifar10 = datasets.cifar10
     (train_images, train_labels), (_, _) = cifar10.load_data()
     train_images = train_images.astype('float32')
-    train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
+    train_images = normalize_inputs(train_images)
     train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).shuffle(
         input_params.buffer_size).batch(
         input_params.batch_size)
