7cc35c152db38f1abf02c990e7cd7a54e9ef39ac
Tomasz Latkowski <tlatkowski@gmail.com>
2020-02-27 19:55:15 +0100

    added yml reader

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,7 @@
+import io
 import json
 
+import yaml
 from easydict import EasyDict as edict
 
 
@@ -7,3 +9,9 @@ def read_config(dataset_type):
     with open('config/{}.json'.format(dataset_type.lower())) as f:
         input_params = edict(json.load(f))
     return input_params
+
+
+def read_yml_config(path_to_config_file: str):
+    with io.open(path_to_config_file) as file:
+        config = yaml.load(file)
+        return config
