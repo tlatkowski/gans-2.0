751f6afcd5637f1e6b06a388634dc3b69f268be1
Tomasz Latkowski <tlatkowski@gmail..com>
2020-06-22 14:33:20 +0200

    added yml reader

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ from models import model_factories
 
 
 def run_experiment(input_args):
-    gan_type = input_args.problem_type.split('_')[0]
     problem_type = input_args.problem_type
+    gan_type = problem_type.split('_')[0]
     problem_params = config.read_config(problem_type)
     dataset = dataset_factory.get_dataset(problem_params, problem_type)
     gan_model = model_factories.gan_model_factory(problem_params, gan_type, input_args)
@@ -17,14 +17,18 @@ def run_experiment(input_args):
 def main():
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--problem_type',
-                        required=True,
-                        help='The problem type',
-                        choices=dataset_factory.dataset_type_values())
+    parser.add_argument(
+        '--problem_type',
+        required=True,
+        help='The problem type',
+        choices=dataset_factory.dataset_type_values(),
+    )
     
-    parser.add_argument('-continue_training',
-                        action='store_true',
-                        help='If set the training process will be continued')
+    parser.add_argument(
+        '-continue_training',
+        action='store_true',
+        help='If set the training process will be continued',
+    )
     
     args = parser.parse_args()
     
--- utils/config.py
+++ utils/config.py
@@ -1,17 +1,8 @@
-import io
-import json
-
 import yaml
 from easydict import EasyDict as edict
 
 
-def read_config(dataset_type):
-    with open('config/{}.json'.format(dataset_type.lower())) as f:
-        input_params = edict(json.load(f))
-    return input_params
-
-
-def read_yml_config(path_to_config_file: str):
-    with io.open(path_to_config_file) as file:
-        config = yaml.load(file)
+def read_config(problem_type):
+    with open('config/{}.yml'.format(problem_type.lower())) as f:
+        config = edict(yaml.load(f))
         return config
