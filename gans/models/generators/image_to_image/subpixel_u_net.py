0f7c532d92b2f409128f43bb582d8871629fa891
Tomasz Latkowski <tlatkowski@gmail.com>
2020-06-25 16:28:30 +0200

    refactored generators

--- gans/models/generators/image_to_image/subpixel_u_net.py
+++ gans/models/generators/image_to_image/subpixel_u_net.py
@@ -1,35 +1,24 @@
 import tensorflow_addons as tfa
-from tensorflow.python.keras import Input, Model
+from easydict import EasyDict as edict
+from tensorflow.python.keras import Input
+from tensorflow.python.keras import Model
 from tensorflow.python.keras import layers
 
 from gans.layers import advanced_layers
+from gans.models.generators import generator
 
 
-class UNetSubpixelGenerator:
-    
+class UNetSubpixelGenerator(generator.Generator):
+
     def __init__(
             self,
+            model_parameters: edict,
     ):
-        self._model = self.create_model()
-    
-    def __call__(self, inputs, **kwargs):
-        return self._model(inputs=inputs, **kwargs)
-    
-    @property
-    def trainable_variables(self):
-        return self._model.trainable_variables
-    
-    @property
-    def model(self):
-        return self._model
-    
-    @property
-    def num_channels(self):
-        return self._model.output_shape[-1]
-    
-    def create_model(self):
+        super().__init__(model_parameters)
+
+    def define_model(self):
         input_images = Input(shape=[256, 256, 3])
-        
+
         x1 = layers.Conv2D(
             filters=64,
             kernel_size=(7, 7),
@@ -39,7 +28,7 @@ class UNetSubpixelGenerator:
         )(input_images)
         x1 = tfa.layers.InstanceNormalization()(x1)
         x1 = layers.ReLU()(x1)
-        
+
         x2 = layers.Conv2D(
             filters=128,
             kernel_size=(3, 3),
@@ -49,7 +38,7 @@ class UNetSubpixelGenerator:
         )(x1)
         x2 = tfa.layers.InstanceNormalization()(x2)
         x2 = layers.ReLU()(x2)
-        
+
         x3 = layers.Conv2D(
             filters=256,
             kernel_size=(3, 3),
@@ -59,7 +48,7 @@ class UNetSubpixelGenerator:
         )(x2)
         x3 = tfa.layers.InstanceNormalization()(x3)
         x3 = layers.ReLU()(x3)
-        
+
         x4 = layers.Conv2D(
             filters=512,
             kernel_size=(3, 3),
@@ -69,11 +58,10 @@ class UNetSubpixelGenerator:
         )(x3)
         x4 = tfa.layers.InstanceNormalization()(x4)
         x4 = layers.ReLU()(x4)
-        
+
         x5 = layers.UpSampling2D()(x4)
-        # x5 = PS(x4, r=2, batch_size=4)
         x5 = layers.Concatenate()([x5, x3])
-        
+
         x5 = layers.Conv2D(
             filters=256,
             kernel_size=(3, 3),
@@ -83,12 +71,11 @@ class UNetSubpixelGenerator:
         )(x5)
         x5 = tfa.layers.InstanceNormalization()(x5)
         x5 = layers.LeakyReLU(alpha=0.2)(x5)
-        
+
         x6 = layers.UpSampling2D()(x5)
-        # x6 = PS(x5, r=2, batch_size=4)
-        
+
         x6 = layers.Concatenate()([x6, x2])
-        
+
         x6 = layers.Conv2D(
             filters=128,
             kernel_size=(3, 3),
@@ -98,9 +85,8 @@ class UNetSubpixelGenerator:
         )(x6)
         x6 = tfa.layers.InstanceNormalization()(x6)
         x6 = layers.LeakyReLU(alpha=0.2)(x6)
-        
+
         x7 = layers.UpSampling2D()(x6)
-        # x7 = PS(x6, r=2, batch_size=4)
         x7 = layers.Concatenate()([x7, x1])
         x7 = layers.Conv2D(
             filters=64,
@@ -111,9 +97,8 @@ class UNetSubpixelGenerator:
         )(x7)
         x7 = tfa.layers.InstanceNormalization()(x7)
         x7 = layers.LeakyReLU(alpha=0.2)(x7)
-        
+
         x8 = advanced_layers.subpixel_upsampling(x7, r=2)
-        # x8 = layers.UpSampling2D()(x7)
         x8 = layers.Concatenate()([x8, input_images])
         x8 = layers.Conv2D(
             filters=32,
@@ -124,7 +109,7 @@ class UNetSubpixelGenerator:
         )(x8)
         x8 = tfa.layers.InstanceNormalization()(x8)
         x8 = layers.LeakyReLU(alpha=0.2)(x8)
-        
+
         x9 = layers.Conv2D(
             filters=3,
             kernel_size=(5, 5),
@@ -133,6 +118,6 @@ class UNetSubpixelGenerator:
             use_bias=False,
             activation='tanh',
         )(x8)
-        
-        model = Model(name='Generator', inputs=input_images, outputs=x9)
+
+        model = Model(name=self, inputs=input_images, outputs=x9)
         return model
