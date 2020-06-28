63f14de3ab067c51a8cac18a9bcc0dff472effb6
Tomasz Latkowski <tlatkowski@gmail.com>
2020-06-28 20:38:41 +0200

    extracted optimizers

--- examples/conditional_gan_mnist.py
+++ examples/conditional_gan_mnist.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from easydict import EasyDict as edict
 
 from gans.datasets import mnist
@@ -7,28 +8,37 @@ from gans.models.generators.latent_to_image import random_to_image
 from gans.trainers import conditional_gan_trainer
 
 model_parameters = edict({
-    'img_height': 28,
-    'img_width': 28,
-    'num_channels': 1,
-    'batch_size': 16,
-    'num_epochs': 10,
-    'buffer_size': 1000,
-    'hidden_size': 100,
-    'learning_rate_generator': 0.0001,
+    'img_height':                  28,
+    'img_width':                   28,
+    'num_channels':                1,
+    'batch_size':                  16,
+    'num_epochs':                  10,
+    'buffer_size':                 1000,
+    'hidden_size':                 100,
+    'learning_rate_generator':     0.0001,
     'learning_rate_discriminator': 0.0001,
-    'save_images_every_n_steps': 10
+    'save_images_every_n_steps':   10
 })
 
 generator = random_to_image.RandomToImageGenerator(model_parameters)
 discriminator = basic_discriminator.Discriminator(model_parameters)
+
+generator_optimizer = tf.keras.optimizers.Adam(
+    learning_rate=model_parameters.learning_rate_generator,
+    beta_1=0.5,
+)
+discriminator_optimizer = tf.keras.optimizers.Adam(
+    learning_rate=model_parameters.learning_rate_discriminator,
+    beta_1=0.5,
+)
 
 gan_trainer = conditional_gan_trainer.ConditionalGANTrainer(
     batch_size=model_parameters.batch_size,
     generator=generator,
     discriminator=discriminator,
     dataset_type='VANILLA_MNIST',
-    learning_rate_generator=model_parameters.learning_rate_generator,
-    learning_rate_discriminator=model_parameters.learning_rate_discriminator,
+    generator_optimizer=generator_optimizer,
+    discriminator_optimizer=discriminator_optimizer,
     continue_training=False,
     save_images_every_n_steps=model_parameters.save_images_every_n_steps,
 )
