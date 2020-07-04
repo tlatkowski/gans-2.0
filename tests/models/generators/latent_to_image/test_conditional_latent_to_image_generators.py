9ffd08ef4b24989316171665635950b84fddb88c
Tomasz Latkowski <tlatkowski@gmail.com>
2020-07-04 09:07:55 +0200

    refactored tests

---
+++ tests/models/generators/latent_to_image/test_conditional_latent_to_image_generators.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from easydict import EasyDict as edict
+
+from gans.models.generators.latent_to_image import conditional_latent_to_image
+
+
+class TestConditionalLatentToImageGenerators(tf.test.TestCase):
+
+    def test_conditional_random_to_image_generator_output_shape(self):
+        input_params = edict({
+            'latent_size': 100,
+            'num_classes': 10
+        })
+        g = conditional_latent_to_image.RandomToImageConditionalGenerator(input_params)
+        z = tf.random.normal(shape=(1, 100))
+        class_id = tf.zeros(shape=(1,))
+        output_img = g([z, class_id])
+        expected_shape = (1, 28, 28, 1)
+        self.assertEqual(output_img.shape, expected_shape)
---
+++ tests/models/generators/latent_to_image/test_latent_to_image_generators.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from easydict import EasyDict as edict
+
+from gans.models.generators.latent_to_image import latent_to_image
+
+
+class TestLatentToImageGenerators(tf.test.TestCase):
+
+    def test_random_to_image_generator_output_shape(self):
+        input_params = edict({
+            'latent_size': 100
+        })
+        g = latent_to_image.RandomToImageGenerator(input_params)
+        z = tf.random.normal(shape=[1, 100])
+        output_img = g(z)
+        expected_shape = (1, 28, 28, 1)
+        self.assertEqual(output_img.shape, expected_shape)
