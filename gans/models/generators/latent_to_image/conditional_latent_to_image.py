9e249374d93fd78bbcc8fc07bcd098be0246b6f6
Tomasz Latkowski <tlatkowski@gmail.com>
2020-07-04 08:32:46 +0200

    refactored generators

--- gans/models/generators/latent_to_image/conditional_latent_to_image.py
+++ gans/models/generators/latent_to_image/conditional_latent_to_image.py
@@ -42,3 +42,133 @@ class RandomToImageConditionalGenerator(model.Model):
 
         model = Model(name=self.model_name, inputs=[z, class_id], outputs=x)
         return model
+
+
+class RandomToImageCifar10CConditionalGenerator(model.Model):
+
+    def __init__(
+            self,
+            model_parameters: edict,
+    ):
+        super().__init__(model_parameters)
+
+    def define_model(self):
+        z = Input(shape=[self.model_parameters.hidden_size])
+        class_id = Input(shape=[1])
+
+        embedded_id = layers.Embedding(input_dim=10, output_dim=50)(class_id)
+        embedded_id = layers.Dense(units=8 * 8)(embedded_id)
+        embedded_id = layers.Reshape(target_shape=(8, 8, 1))(embedded_id)
+
+        x = layers.Dense(units=8 * 8 * 256, use_bias=False)(z)
+        x = layers.BatchNormalization(momentum=0.9)(x)
+        x = layers.LeakyReLU(alpha=0.1)(x)
+        x = layers.Reshape((8, 8, 256))(x)
+
+        inputs = layers.Concatenate(axis=3)([x, embedded_id])
+
+        x = layers.Conv2DTranspose(128, kernel_size=(4, 4), strides=(2, 2), padding='same', use_bias=False)(inputs)
+        x = layers.BatchNormalization(momentum=0.9)(x)
+        x = layers.LeakyReLU(alpha=0.1)(x)
+
+        x = layers.Conv2D(128, kernel_size=(5, 5), strides=(1, 1), padding='same', use_bias=False)(x)
+        x = layers.BatchNormalization(momentum=0.9)(x)
+        x = layers.LeakyReLU(alpha=0.1)(x)
+
+        x = layers.Conv2DTranspose(128, kernel_size=(4, 4), strides=(2, 2), padding='same', use_bias=False)(x)
+        x = layers.BatchNormalization(momentum=0.9)(x)
+        x = layers.LeakyReLU(alpha=0.1)(x)
+
+        x = layers.Conv2D(128, kernel_size=(5, 5), strides=(1, 1), padding='same', use_bias=False)(x)
+        x = layers.BatchNormalization(momentum=0.9)(x)
+        x = layers.LeakyReLU(alpha=0.1)(x)
+
+        x = layers.Conv2D(128, kernel_size=(5, 5), strides=(1, 1), padding='same', use_bias=False)(x)
+        x = layers.BatchNormalization(momentum=0.9)(x)
+        x = layers.LeakyReLU(alpha=0.1)(x)
+
+        x = layers.Conv2D(3, kernel_size=(5, 5), strides=(1, 1), padding='same', use_bias=False, activation='tanh')(x)
+
+        model = Model(name=self.model_name, inputs=[z, class_id], outputs=x)
+        return model
+
+
+class RandomToImageNNUpsamplingCifar10CConditionalGenerator(model.Model):
+
+    def __init__(
+            self,
+            model_parameters: edict,
+    ):
+        super().__init__(model_parameters)
+
+    def define_model(self):
+        z = Input(shape=[self.model_parameters.hidden_size])
+        class_id = Input(shape=[1])
+
+        embedded_id = layers.Embedding(input_dim=10, output_dim=50)(class_id)
+        embedded_id = layers.Dense(units=8 * 8)(embedded_id)
+        embedded_id = layers.Reshape(target_shape=(8, 8, 1))(embedded_id)
+
+        x = layers.Dense(units=8 * 8 * 256, use_bias=False)(z)
+        x = layers.BatchNormalization()(x)
+        x = layers.LeakyReLU()(x)
+
+        x = layers.Reshape((8, 8, 256))(x)
+
+        inputs = layers.Concatenate(axis=3)([x, embedded_id])
+
+        x = layers.Conv2D(128, (5, 5), strides=(1, 1), padding='same', use_bias=False)(inputs)
+        x = layers.BatchNormalization()(x)
+        x = layers.LeakyReLU()(x)
+        x = layers.UpSampling2D()(x)
+
+        x = layers.Conv2D(64, (5, 5), strides=(1, 1), padding='same', use_bias=False)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.LeakyReLU()(x)
+        x = layers.UpSampling2D()(x)
+
+        x = layers.Conv2D(3, (5, 5), strides=(1, 1), padding='same', use_bias=False, activation='tanh')(x)
+
+        model = Model(name=self.model_name, inputs=[z, class_id], outputs=x)
+        return model
+
+
+class RandomToImageNNUpSamplingConditionalGenerator(model.Model):
+
+    def __init__(
+            self,
+            model_parameters: edict,
+    ):
+        super().__init__(model_parameters)
+
+    def define_model(self):
+        z = Input(shape=[self.model_parameters.hidden_size])
+        class_id = Input(shape=[1])
+
+        embedded_id = layers.Embedding(input_dim=10, output_dim=50)(class_id)
+        embedded_id = layers.Dense(units=7 * 7)(embedded_id)
+        embedded_id = layers.Reshape(target_shape=(7, 7, 1))(embedded_id)
+
+        x = layers.Dense(units=7 * 7 * 256, use_bias=False)(z)
+        x = layers.BatchNormalization()(x)
+        x = layers.LeakyReLU()(x)
+
+        x = layers.Reshape((7, 7, 256))(x)
+
+        inputs = layers.Concatenate(axis=3)([x, embedded_id])
+
+        x = layers.Conv2D(128, (5, 5), strides=(1, 1), padding='same', use_bias=False)(inputs)
+        x = layers.BatchNormalization()(x)
+        x = layers.LeakyReLU()(x)
+        x = layers.UpSampling2D()(x)
+
+        x = layers.Conv2D(64, (5, 5), strides=(1, 1), padding='same', use_bias=False)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.LeakyReLU()(x)
+
+        x = layers.UpSampling2D()(x)
+
+        x = layers.Conv2D(1, (5, 5), strides=(1, 1), padding='same', use_bias=False, activation='tanh')(x)
+
+        model = Model(name=self.model_name, inputs=[z, class_id], outputs=x)
+        return model
--- gans/models/generators/latent_to_image/latent_to_image.py
+++ gans/models/generators/latent_to_image/latent_to_image.py
@@ -34,3 +34,65 @@ class RandomToImageGenerator(model.Model):
 
         model = Model(name=self.model_name, inputs=z, outputs=x)
         return model
+
+
+class RandomToImageCifar10Generator(model.Model):
+
+    def __init__(
+            self,
+            model_parameters: edict,
+    ):
+        super().__init__(model_parameters)
+
+    def define_model(self):
+        z = Input(shape=[self.model_parameters.hidden_size])
+
+        x = layers.Dense(units=8 * 8 * 256, use_bias=False)(z)
+        x = layers.BatchNormalization()(x)
+        x = layers.LeakyReLU()(x)
+
+        x = layers.Reshape((8, 8, 256))(x)
+        x = layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.LeakyReLU()(x)
+
+        x = layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.LeakyReLU()(x)
+
+        x = layers.Conv2DTranspose(3, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh')(x)
+
+        model = Model(name=self.model_name, inputs=z, outputs=x)
+        return model
+
+
+class RandomToImageCifar10NearestNeighborUpSamplingGenerator(model.Model):
+
+    def __init__(
+            self,
+            model_parameters: edict,
+    ):
+        super().__init__(model_parameters)
+
+    def define_model(self):
+        z = Input(shape=[self.model_parameters.hidden_size])
+
+        x = layers.Dense(units=8 * 8 * 256, use_bias=False)(z)
+        x = layers.BatchNormalization()(x)
+        x = layers.LeakyReLU()(x)
+
+        x = layers.Reshape((8, 8, 256))(x)
+        x = layers.Conv2D(128, (5, 5), strides=(1, 1), padding='same', use_bias=False)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.LeakyReLU()(x)
+
+        x = layers.UpSampling2D()(x)
+        x = layers.Conv2D(64, (5, 5), strides=(1, 1), padding='same', use_bias=False)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.LeakyReLU()(x)
+
+        x = layers.UpSampling2D()(x)
+        x = layers.Conv2D(3, (5, 5), strides=(1, 1), padding='same', use_bias=False, activation='tanh')(x)
+
+        model = Model(name=self.model_name, inputs=z, outputs=x)
+        return model
